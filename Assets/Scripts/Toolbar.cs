59109324f46dd7fc5eb00c2d1b4246e2b92677e9
Desaster28 <Desaster28@users.noreply.github.com>
2019-12-10 15:32:29 +0100

    Scrollable Toolbar

---
+++ Assets/Scripts/Toolbar.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toolbar : MonoBehaviour {
+    [SerializeField]
+    public RectTransform highlight;
+    public ItemSlot[] itemSlots;
+
+    int slotIndex = 0;
+
+    private void Start() {
+
+    }
+
+    private void Update() {
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scroll != 0) {
+
+            if (scroll > 0)
+                slotIndex--;
+            else
+                slotIndex++;
+
+            if (slotIndex > itemSlots.Length - 1)
+                slotIndex = 0;
+            if (slotIndex < 0)
+                slotIndex = itemSlots.Length - 1;
+
+            highlight.position = itemSlots[slotIndex].icon.transform.position;
+
+        }
+    }
+}
+
+[System.Serializable]
+public class ItemSlot {
+
+    public byte itemID;
+    public Image icon;
+
+}
