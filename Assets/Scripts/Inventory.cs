11dacfc776cb1828a14e30d1c080cc47cc87bc39
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2020-01-06 00:52:42 +0100

    Added Inventory

---
+++ Assets/Scripts/Inventory.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+  
+  
+  public bool inventoryEnabled;
+  public GameObject Inventory;
+  
+  private int allSlots;
+  private int enabledSlots;
+  private GameObject[] slot;
+  public GameObject slotHolder;
+  
+  
+  void Start(){
+	allSlots = 26;
+	slot = new GameObject[allSlots];
+	for(int i = 0;i< allSlots;i++){
+		slot[i] = slotHolder.transform.GetChild(i).gameObject;
+	}
+  }
+  
+  
+  
+  void Update(){
+	if(Input.GetKeyDown(KeyCode.I)){
+		inventoryEnabled = !inventoryEnabled;
+	}
+	
+	if(inventoryEnabled){
+		Inventory.SetActive(true);
+	}else{
+		Inventory.SetActive(false);
+	}
+  }
+  
+}
