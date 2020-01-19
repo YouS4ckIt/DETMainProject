6e1838253ea6dba50f0da4af0a6e6fc11b78a9c4
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2020-01-19 18:07:08 +0100

    Fixed Selection/added 3 Models/   INGAME UI needs to be finished and Shooting synchro is missing

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -26,4 +26,5 @@ public class MenuManager : MonoBehaviour
 
 
 
+
 }
---
+++ Assets/StoreButton.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class StoreButton : MonoBehaviour
+{
+    public Transform centerStore;
+    public Transform storeContainer;
+    public void OnClickCharacter()
+    {
+        float dis = centerStore.position.x - transform.position.x;
+        StoreController.newPose = new Vector3(storeContainer.position.x + dis, storeContainer.position.y, storeContainer.position.z);
+        StoreController.SelectMove = true;
+    }
+    public void SelectCharacter(int whichCharacter)
+    {
+        Debug.Log("SelectedCharacter is : " + whichCharacter);
+    }
+}
