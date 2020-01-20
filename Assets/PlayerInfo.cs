c8824bc0b425ef4001a7dc6eb6a3a7470f667453
YouS4ckIt <33163253+YouS4ckIt@users.noreply.github.com>
2020-01-20 01:19:28 +0100

    characterselect scroll

---
+++ Assets/PlayerInfo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInfo : MonoBehaviour
+{
+
+    public static PlayerInfo PI;
+
+    public int selectedCharacter;
+
+    public GameObject[] allCharacters;
+
+    private void OnEnable()
+    {
+        if(PlayerInfo.PI == null)
+        {
+            PlayerInfo.PI = this;
+        }
+        else
+        {
+            if(PlayerInfo.PI != this)
+            {
+                Destroy(PlayerInfo.PI.gameObject);
+            }
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("MyCharacter"))
+        {
+            selectedCharacter = PlayerPrefs.GetInt("MyCharacter");
+        }
+        else
+        {
+            selectedCharacter = 0;
+            PlayerPrefs.SetInt("MyCharacter", selectedCharacter);
+        }
+    }
+
+
+
+
+}
