9d4f761d3b33780c65d50a5fb9a65364903198c2
YouSuckIt <diman2007cool@rambler.ru>
2020-01-21 13:38:43 +0100

    new inventory

--- Assets/UIScripts/CreativeInventory.cs
+++ Assets/UIScripts/CreativeInventory.cs
@@ -10,11 +10,12 @@ public class CreativeInventory : MonoBehaviour
     List<ItemSlot> slots = new List<ItemSlot>();
     private void Start()
     {
-        for(int i = 1; i < 55; i++)
+        for(int i = 1; i < 5; i++)
         {
             GameObject newSlot = Instantiate(slotPrefab, transform);
             //ItemStack stack = new ItemStack(toolbar.items[0], 64);
-            ItemSlot slot = new ItemSlot(newSlot.GetComponent<UIItemSlot>());
+            ItemStack stack = new ItemStack(toolbar.items[0], 1);
+            ItemSlot slot = new ItemSlot(newSlot.GetComponent<UIItemSlot>(), stack);
             //slot.isCreative = true;
         }
     }
